b274f8cde9bf2a84015f52c8e4f5e50bf7224e18
Shinodari <shinodari_bb@hotmail.com>
2024-05-15 21:58:25 +0700

    feat: first ref #1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <title>Warehouse IO Monitor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        Test
+    </header>
+    <section>
+
+    </section>
+    <footer>
+        <img src="picture/footer.png">
+    </footer>
+</body>
+</html>
