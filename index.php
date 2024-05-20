28d3ca988faf6e776796973ee7a8071701903b62
Shinodari <shinodari_bb@hotmail.com>
2024-05-20 23:30:03 +0700

    uncomplete

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <!DOCTYPE html>
 <head>
     <title>Warehouse IO Monitor</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/jquery-3.7.1.min.js"></script>
+    <script>
+        let orderDisplay;
+        $(function(){
+            $.ajax({
+                url:'lib/getorderdisplay.php',
+                type:'post',
+                dataType:'json',
+                success:callbackOrderDisplay,
+                timeout:'10000',
+                error:function(xhr, testStatus){
+                    alert('Get order Display is error! \nPlease contact Admin')
+                }
+            });
+        });
+
+        function callbackOrderDisplay(result){
+            orderDisplay = result;
+        }
+    </script>
 </head>
 <body>
     <header>
         Test
     </header>
     <section>
-
+        <div></div>
     </section>
     <footer>
         <img src="picture/footer.png">
