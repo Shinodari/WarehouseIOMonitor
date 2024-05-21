636b859dbb01784eec58ec6c5b0b52238eb9ec61
Shinodari <shinodari_bb@hotmail.com>
2024-05-21 22:45:41 +0700

    feat(order display): prepare data for display ref: #1

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
+<?php
+include 'lib/global_variable.php';
+$link = @mysqli_connect($host, $username, $password, $database)
+    or die("Can't connect the database!/Please contact the admin.");
+
+$orderDisplayList = array();
+$sql = "SELECT * FROM monitor_order";
+$rs = mysqli_query($link, $sql);
+while($data = mysqli_fetch_array($rs)){
+    $orderDisplayList[(int)$data["ID"]] = $data["supplierID"];
+}
+mysqli_close($link);
+?>
 <!DOCTYPE html>
 <head>
     <title>Warehouse IO Monitor</title>
     <link rel="stylesheet" href="./css/style.css">
     <script src="./js/jquery-3.7.1.min.js"></script>
     <script>
-        let orderDisplay;
+        const orderDisplayList =[];
+        orderDisplayList[0] = "0";
+        <?php
+            for($i = 1; $i <= count($orderDisplayList); $i++){
+                echo "\t\torderDisplayList[{$i}] = \"{$orderDisplayList[$i]}\";\n";
+            }
+        ?>
         $(function(){
             $.ajax({
                 url:'lib/getorderdisplay.php',
@@ -12,23 +31,25 @@
                 dataType:'json',
                 success:callbackOrderDisplay,
                 timeout:'10000',
-                error:function(xhr, testStatus){
-                    alert('Get order Display is error! \nPlease contact Admin')
+                error:function(xhr, errorStatus){
+                    alert('Get order Display is ' + errorStatus + '! \nPlease contact Admin');
                 }
             });
+
+            //$('.suplierName').text(orderDisplayList.1);
         });
 
         function callbackOrderDisplay(result){
-            orderDisplay = result;
+            orderDisplayList = result;
         }
     </script>
 </head>
 <body>
     <header>
-        Test
+        <div class="suplierName"></div>
     </header>
     <section>
-        <div></div>
+        <div>Section<br>Test<br></div>
     </section>
     <footer>
         <img src="picture/footer.png">
